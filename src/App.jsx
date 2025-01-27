93820ed4f781a6813f984bf434bcac476dbf4502
Akash Sapkal <akashsapkal7620@gmail.com>
2025-01-27 18:06:41 +0530

    Do Basic needed Installations for project setup like tailwind, json-server and auth, redux, routers and google fonts and bootstarp icons

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+     <h1
+     className='w-96 h-28 text-white mx-auto my-11 text-center bg-akash-100'
+     >Akash Sapkal
+     <i className="bi bi-4-circle"></i>
+     </h1>
+    </>
+  )
+}
+
+export default App
