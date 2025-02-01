e8e0b3d5b9ba4efc53c6bc6576b8fca5218741a3
Akash Sapkal <akashsapkal7620@gmail.com>
2025-02-01 23:38:04 +0530

    update all routes

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,14 +1,17 @@
-
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages";
-import { ProductList } from "../pages/Product/ProductList";
-export const AllRoutes=()=>{
-    return (
-        <div className="dark:bg-gray-600 pb-4">
-            <Routes>
-                <Route path="/" element={<HomePage />}/>
-                <Route path="/products" element={<ProductList />}/>
-            </Routes>
-        </div>
-    )
-}
+import { ProductList } from "../pages";
+// import { SearchPage } from "../pages";
+import { ProductDetail } from "../pages";
+export const AllRoutes = () => {
+  return (
+    <div className="dark:bg-slate-600 pb-4">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/search" element={<ProductList />} />
+        <Route path="product/:id" element={<ProductDetail />} />
+      </Routes>
+    </div>
+  );
+};
