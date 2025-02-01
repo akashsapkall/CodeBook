0e9f564f20e7c2b32bf117b8f93e61bf90d3c429
Akash Sapkal <akashsapkal7620@gmail.com>
2025-02-01 23:38:35 +0530

    add store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterReducer from "./FilterSlice";
-const store=configureStore({
+export const store=configureStore({
     reducer:{
         filter:filterReducer,
     }
