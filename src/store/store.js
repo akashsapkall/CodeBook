3762ff8bf153970725a5332be49e56c656b72b08
Akash Sapkal <akashsapkal7620@gmail.com>
2025-02-04 20:43:29 +0530

    update store for cartSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filterReducer from "./FilterSlice";
+import cartReducer from "./CartSlice";
 export const store=configureStore({
     reducer:{
         filter:filterReducer,
+        cart:cartReducer,
     }
 });
