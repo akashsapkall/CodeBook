d22500ccc8fb79d5964b36dd52d0052dad5d32c2
Akash Sapkal <akashsapkal7620@gmail.com>
2025-02-04 20:47:17 +0530

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
 import{ store } from "./store/store.js"
 import{ ScrollToTop } from "./components/ScrollToTop.jsx"
 import "./index.css";
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")).render(
       <Router>
     <Provider store={store}>
         <App />
+        <ToastContainer position={"bottom-right"}/>
         <ScrollToTop />
     </Provider>
       </Router>
