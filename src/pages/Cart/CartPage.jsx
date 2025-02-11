14034df940544cbbbedc71ef311466347771d6d9
Akash Sapkal <akashsapkal7620@gmail.com>
2025-02-11 20:37:32 +0530

    update cart page

--- src/pages/Cart/CartPage.jsx
+++ src/pages/Cart/CartPage.jsx
@@ -1,8 +1,10 @@
 import { EmptyCart } from "./components/EmptyCart"
 import { CartList } from "./components/CartList"
 import { useSelector } from "react-redux";
+import { useTitle } from "../../hooks/useTitle";
 export const CartPage=()=>{
     const cartList=useSelector((state)=>state.cart.cartList);
+    useTitle(`Cart(${cartList.length})`);
     return (
        <main>
         {cartList.length!==0?<CartList cartList={cartList}/>:< EmptyCart />}
